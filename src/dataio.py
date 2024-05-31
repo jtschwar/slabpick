8fc4c25b37ab2b5bef5aec76f7e0d884828f8223
Ariana Peck <ariana.peck@cpu-b-4.clusternet>
2024-05-31 16:57:15 -0700

    generate galleries from copicks

--- src/dataio.py
+++ src/dataio.py
@@ -1,5 +1,7 @@
+from copick.impl.filesystem import CopickRootFSSpec
 import numpy as np
 import mrcfile
+import zarr
 
 def load_mrc(filename):
     """ 
@@ -51,3 +53,86 @@ def get_voxel_size(filename, isotropic=True):
     if isotropic:
         return apix[0]
     return apix
+
+class CoPickWrangler:
+    """
+    Utilties to extract information from a copick project. 
+    copick documentation: https://uermel.github.io/copick/
+    """
+    def __init__(self, config):
+        """
+        Parameters
+        ----------
+        config: copick configuration file 
+        """
+        self.root = CopickRootFSSpec.from_file(config)
+
+    def get_run_coords(self, run_name, particle_name):
+        """
+        Extract coordinates for a partciular run.
+
+        Parameters
+        ----------
+        run_name: str, name of run 
+        particle_name: str, name of particle
+
+        Returns
+        -------
+        np.array, shape (n_coords, 3) of coordinates in Angstrom
+        """
+        try:
+            pick = self.root.get_run(run_name).get_picks(particle_name)[0]
+        except AttributeError:
+            print("Run or particle name not found")
+            return np.empty(0)
+        return np.array([(p.location.x, p.location.y, p.location.z) for p in pick.points])
+
+    def get_run_tomogram(self, run_name, voxel_spacing, tomo_type):
+        """
+        Get tomogram for a particular run.
+
+        Parameters
+        ----------
+        run_name: str, name of run 
+        voxel_spacing: float, voxel spacing in Angstrom
+        tomo_type: str, type of tomogram, e.g. denoised
+
+        Returns
+        -------
+        array: zarr.core.Array, tomogram volume
+        """
+        run = self.root.get_run(run_name)
+        tomogram = run.get_voxel_spacing(voxel_spacing).get_tomogram(tomo_type=tomo_type)
+        arrays = list(zarr.open(tomogram.zarr(), "r").arrays())
+        _, array = arrays[0] # 0 corresponds to unbinned
+        return array
+    
+    def get_run_names(self):
+        """
+        Extract all run names.
+
+        Returns
+        -------
+        list, names of all available runs
+        """
+        return [run.name for run in self.root.runs]
+
+    def get_all_coords(self, particle_name):
+        """
+        Extract all coordinates for a particle across a dataset.
+
+        Parameters
+        ----------
+        particle_name: str, name of particle   
+
+        Returns
+        -------
+        d_coords: dict, mapping of run name to particle coordinates
+        """
+        runs = self.get_run_names()
+        d_coords = {}
+        for run in runs:
+            coords = self.get_run_coords(run, 'apo-ferritin')
+            if len(coords) > 0:
+                d_coords[run] = coords
+        return d_coords
--- src/gallery.py
+++ src/gallery.py
@@ -6,7 +6,7 @@ import numpy as np
 import starfile
 import re
 import os
-from dataio import load_mrc, get_voxel_size, save_mrc
+from dataio import *
 
 class Gallery:
     
@@ -25,42 +25,33 @@ class Gallery:
         self.num_particles = 0
         self.tomo_names = []
         self.pick_indices = []
-        self.extract_shape = extract_shape
+        self.shape = extract_shape
         self.coords_scale_factor = coords_scale_factor
-        self.apix = None
         
-    def generate_slabs(self, volume, coords):
+    def generate_slabs(self, vol, coords, vol_name):
         """
         Generate a per-particle slab, i.e. a 2d projection along z
         that is centered around each particle. 
         
         Parameters
         ----------
-        volume: str, path to tomogram in mrc format
-        coords: str, path to coordinates in star format
+        vol: numpy or zarr array, tomogram
+        coords: np.ndarray (n_coords, 3), xyz coordinates in pixels
+        vol_name: str, name of volume
         """
-        vol = load_mrc(volume)
-        if self.apix is None:
-            self.apix = get_voxel_size(volume)
-        shape = (np.array(self.extract_shape)/self.apix).astype(int)
-        self.filler_mean, self.filler_std = vol.mean(), vol.std()
-        
-        picks = starfile.read(coords)
-        coords = np.array([picks.rlnCoordinateX.values,
-                           picks.rlnCoordinateY.values,
-                           picks.rlnCoordinateZ.values]).T.astype(np.float32)
+        self.filler_mean, self.filler_std = np.mean(vol), np.std(vol)
         coords *= self.coords_scale_factor
         
         for i,c in enumerate(coords):
             # crop particle-centered subvolume and project along z
             c = c.astype(int)
-            xstart, xend = c[2]-int(shape[2]/2), c[2]+int(shape[2]/2) if shape[2]%2==0 else c[2]+int(shape[2]/2) + 1
-            ystart, yend = c[1]-int(shape[1]/2), c[1]+int(shape[1]/2) if shape[1]%2==0 else c[1]+int(shape[1]/2) + 1
-            zstart, zend = c[0]-int(shape[0]/2), c[0]+int(shape[0]/2) if shape[0]%2==0 else c[0]+int(shape[0]/2) + 1
+            xstart, xend = c[2]-int(self.shape[2]/2), c[2]+int(self.shape[2]/2) if self.shape[2]%2==0 else c[2]+int(self.shape[2]/2) + 1
+            ystart, yend = c[1]-int(self.shape[1]/2), c[1]+int(self.shape[1]/2) if self.shape[1]%2==0 else c[1]+int(self.shape[1]/2) + 1
+            zstart, zend = c[0]-int(self.shape[0]/2), c[0]+int(self.shape[0]/2) if self.shape[0]%2==0 else c[0]+int(self.shape[0]/2) + 1
             if xstart < 0: xstart = 0
             if ystart < 0: ystart = 0
             if zstart < 0: zstart = 0
-            subvol = vol[xstart:xend,ystart:yend,zstart:zend]
+            subvol = np.array(vol[xstart:xend,ystart:yend,zstart:zend])
             
             if np.any(np.array(subvol.shape)==0) or np.any(c<0):
                 print(f"Skipping entry with coordinates {coords[i]} due to out of bounds error")
@@ -69,38 +60,38 @@ class Gallery:
             
             # fill any missing rows/columns if particle is along tomogram x/y edge 
             std_scale_factor = 10
-            if projection.shape[0] != shape[1]:
+            if projection.shape[0] != self.shape[1]:
                 edge = projection.shape[0]
                 filler = gaussian_filter(np.random.normal(loc=self.filler_mean, scale=std_scale_factor*self.filler_std, 
-                                                          size=(shape[1]-edge, projection.shape[1])), sigma=1.1)
+                                                          size=(self.shape[1]-edge, projection.shape[1])), sigma=1.1)
                 if ystart == 0:
                     projection = np.vstack((filler, projection))
-                    if shape[1]-edge > 4:
-                        projection[shape[1]-edge-1:shape[1]-edge+1] = gaussian_filter(projection[shape[1]-edge-2:shape[1]-edge+2], sigma=1.1)[1:-1]
+                    if self.shape[1]-edge > 4:
+                        projection[self.shape[1]-edge-1:self.shape[1]-edge+1] = gaussian_filter(projection[self.shape[1]-edge-2:self.shape[1]-edge+2], sigma=1.1)[1:-1]
                 else:
                     projection = np.vstack((projection, filler))
-                    if shape[1]-edge > 4:
+                    if self.shape[1]-edge > 4:
                         projection[edge-1:edge+1] = gaussian_filter(projection[edge-2:edge+2], sigma=1.1)[1:-1]
                         
-            if projection.shape[1] != shape[0]:
+            if projection.shape[1] != self.shape[0]:
                 edge = projection.shape[1]
                 filler = gaussian_filter(np.random.normal(loc=self.filler_mean, scale=std_scale_factor*self.filler_std,
-                                                          size=(projection.shape[0], shape[0]-edge)), sigma=1.1)
+                                                          size=(projection.shape[0], self.shape[0]-edge)), sigma=1.1)
                 if zstart == 0:
                     projection = np.hstack((filler, projection))
-                    if shape[0]-edge > 4:
-                        projection[:,shape[0]-edge-1:shape[0]-edge+1] = gaussian_filter(projection[:,shape[0]-edge-2:shape[0]-edge+2], sigma=1.1)[:,1:-1]
+                    if self.shape[0]-edge > 4:
+                        projection[:,self.shape[0]-edge-1:self.shape[0]-edge+1] = gaussian_filter(projection[:,self.shape[0]-edge-2:self.shape[0]-edge+2], sigma=1.1)[:,1:-1]
                 else:
                     projection = np.hstack((projection, filler))
-                    if shape[0]-edge > 4:
+                    if self.shape[0]-edge > 4:
                         projection[:,edge-1:edge+1] = gaussian_filter(projection[:,edge-2:edge+2], sigma=1.1)[:,1:-1]
             
             self.minislabs[self.num_particles] = projection
-            self.tomo_names.append(picks.rlnMicrographName[i])
+            self.tomo_names.append(vol_name)
             self.pick_indices.append(i)
             self.num_particles += 1
         
-    def generate_galleries(self, gshape, outdir, filename=None):
+    def generate_galleries(self, gshape, apix, outdir, filename=None):
         """
         Generate galleries by tiling the minislabs and save the resulting projected
         particle mosaics in mrc format and a corresponding bookkeeping file as csv.
@@ -108,6 +99,7 @@ class Gallery:
         Parameters
         ----------
         gshape: tuple, number of particles along (rows, cols)
+        apix: float, pixel size in Angstrom   
         outdir: str, output directory for gallery mrcs and bookkeeping csv
         filename: str, specific prefix to include in output files
         """
@@ -137,7 +129,7 @@ class Gallery:
                         gallery_idx[counter], row_idx[counter], col_idx[counter] = nm, i, j
                         counter += 1
             if filename is None: filename = "gallery"
-            save_mrc(gallery, os.path.join(outdir, f"{filename}_{nm:03d}.mrc"), apix=self.apix)
+            save_mrc(gallery, os.path.join(outdir, f"{filename}_{nm:03d}.mrc"), apix=apix)
 
         # save bookkeeping file in csv format
         df = pd.DataFrame({'tomogram': self.tomo_names,
@@ -147,3 +139,34 @@ class Gallery:
                            'col': col_idx})
         if filename is None: filename = "bookkeeping"
         df.to_csv(os.path.join(outdir, f"{filename}.csv"), index=False)
+
+def generate_from_copick(config: str,
+                         out_dir: str,
+                         particle_name: str,
+                         voxel_spacing: float,
+                         tomo_type: str,
+                         extract_shape: tuple,
+                         gallery_shape: tuple = (16,15)):
+    """
+    Generate galleries from a copick project.
+
+    Parameters
+    ----------
+    config: copick configuration file 
+    out_dir: directory to write galleries and bookkeeping file to
+    particle_name: particle name
+    voxel_spacing: voxel spacing in Angstrom
+    tomo_type: type of tomogram, e.g. 'denoised'
+    extract_shape: subvolume extraction shape in Angstrom
+    gallery_shape: number of particles along gallery (row,col)
+    """
+    cp_interface = CoPickWrangler(config)
+    coords = cp_interface.get_all_coords(particle_name)
+    extract_shape = tuple((np.array(extract_shape)/voxel_spacing).astype(int))
+    
+    montage = Gallery(extract_shape)
+    for run_name in coords.keys():
+        volume = cp_interface.get_run_tomogram(run_name, voxel_spacing, tomo_type)
+        coords_pixels = coords[run_name]/voxel_spacing
+        montage.generate_slabs(volume, coords_pixels, run_name)
+    montage.generate_galleries(gallery_shape, voxel_spacing, out_dir)
